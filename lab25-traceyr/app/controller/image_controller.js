02132c8add60214ef813faa7e190cde02138423c
Tracey <radcliffe.tracey@gmail.com>
2016-09-08 12:22:20 -0700

    almost working

--- lab25-traceyr/app/controller/image_controller.js
+++ lab25-traceyr/app/controller/image_controller.js
@@ -13,18 +13,6 @@ module.exports = function(app) {
         'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRa4N5QWH3ZxeHjZYt3RB7iWRj1GXRI_LQzTQf9CuspJx7Cfs_8UA'
       ],
       display: true
-    },
-    {
-      id: 2,
-      title: 'Corgi Album',
-      description: 'An album of argurably the cutests dogs ever',
-      images: [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvImmIxGEkp4Cs9zmXFWxQUbpjkXyAoy8h02p07cMr3sMhsrH',
-        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSIY7ZUqKIzW5ISQ4EiAYCNgIcVnaoeYj5BRIEPtaHAVPfjFGBy',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD2JpdXRjUSq2FPYvstqecIvrOHOmKVGEmUmqy2362Nrr9FbV-',
-        'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRa4N5QWH3ZxeHjZYt3RB7iWRj1GXRI_LQzTQf9CuspJx7Cfs_8UA'
-      ],
-      display: true
     }];
   });
 };
